3652e6420b512f814a3f24d7256de7ca3cff5870
Dāvis Naglis <davisnaglis2@gmail.com>
2017-02-27 01:08:10 +0200

    Post and User relationship
    Fully functioning one-to-many for now.
    Altered route keyname to pick up by 'slug'.
    Migrations for posts table and forgotten user_id column.

---
+++ app/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['title', 'slug', 'body'];
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+    public function getRouteKeyName() {
+    	return 'slug';
+    }
+}
